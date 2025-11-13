d0e10b7603da656340185c150832aa94af963c0c
unknown <shulevskaelena@gmail.com>
2025-11-13 23:26:42 +0100

    initial

---
+++ src/main/java/com/example/mvp_service_2/MvpService2Application.java
@@ -0,0 +1,13 @@
+package com.example.mvp_service_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpService2Application.class, args);
+	}
+
+}
